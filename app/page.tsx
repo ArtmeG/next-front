fa1854ef6411ef87120175393dd2419137becc1b
agordiychuk <agordiychuk@techmatesgroup.com>
2025-04-14 23:10:40 +0200

    add counter

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,49 @@
-import {ReactNode} from "react";
+"use client";
+import { ReactNode, useState } from "react";
+import style from "./styles.module.scss";
 
-
-type FirstType = string
-interface ISecond  {}
+type FirstType = string;
+interface ISecond {}
 
 // union type
 type IdType = string | number;
 
 // literal type
-type LiterType = 'up' | 'down';
-
+type LiterType = "up" | "down";
 
 //
 interface IA {
-  title: string
-  test?: string // und.. || string
-  children: IA
+    title: string;
+    test?: string; // und.. || string
+    children: IA;
 }
 
 interface IA {
-  name?: string
+    name?: string;
 }
 // IA {title: string;  name: string;}
 
-const TTT_UUU: IA[] = [{title: '1'}, {title: '12'}, {title: '13'}]
+const TTT_UUU: IA[] = [{ title: "1" }, { title: "12" }, { title: "13" }];
 
 // TTT_UUU.push({title: 2} as unknown as IA)
-TTT_UUU.push({title: 2} as any)
+TTT_UUU.push({ title: 2 } as any);
 
 interface Props {
-  children: ReactNode
+    children: ReactNode;
 }
 
 export default function Home({}: Props) {
-return (<>
+    const [counter, setCounter] = useState<number>(0);
 
-
-</>)
+    return (
+        <>
+            <button className={style.btn} onClick={() => setCounter(counter + 1)}>
+                Increment
+            </button>
+            <button className={style.btn} onClick={() => setCounter(counter - 1)}>
+                Decrement
+            </button>
+            <div style={{ margin: "10px" }}>{counter}</div>
+        </>
+    );
 }
