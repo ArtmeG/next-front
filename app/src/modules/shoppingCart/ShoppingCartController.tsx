e772a16fb1a81c6e64ddddb6abd94b5e716ccaa7
Dev <frontend.developer.2022@gmail.com>
2025-05-02 12:43:36 +0300

    Context.Provider розуміння рендерів

--- app/src/modules/shoppingCart/ShoppingCartController.tsx
+++ app/src/modules/shoppingCart/ShoppingCartController.tsx
@@ -1,8 +1,10 @@
 "use client";
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import ShoppingCartViewModel from "@/app/src/modules/shoppingCart/ShoppingCartViewModel";
 import ShoppingCart from "@/app/src/modules/shoppingCart/components/ShoppingCart/ShoppingCart";
 import { ShoppingCartContext } from "./context/ShoppingCartContext";
+
+const ddfdf = {};
 
 const ShoppingCartController = () => {
     const vm = useMemo(() => {
@@ -17,7 +19,14 @@ const ShoppingCartController = () => {
         };
     }, []);
 
+    // const ddd = useRef({ onAdd: setSss, getShoppingCart: sss });
+    // const ddd = useMemo(() => ({ onAdd: setSss, getShoppingCart: sss }), []);
+
     return (
+        // <ShoppingCartContext.Provider value={ddd.current}>
+        // <ShoppingCartContext.Provider value={ddd}>
+        // <ShoppingCartContext.Provider value={{}}>
+        // <ShoppingCartContext.Provider value={ddfdf}>
         <ShoppingCartContext.Provider value={vm}>
             <ShoppingCart />
         </ShoppingCartContext.Provider>
